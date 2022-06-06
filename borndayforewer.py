5d14c0b704e8a01a40f51528d40f3e57c959e96a
Stanislav Permyakov <stas.band23@gmail.com>
2022-06-06 19:46:00 +0300

    Initial commit

---
+++ borndayforewer.py
@@ -0,0 +1,21 @@
+# Модуль 5
+print(" В каком году родился А.С Пушкин?")
+age = int(input("Введите год рождения Пушкина: "))
+while age != 1799:
+    age = int(input("Попробуй еще!"))
+    if age == 1799:
+        day = int(input("Введите день рождения Пушкина: "))
+        while day != 27:
+            day = int(input("Попробуй еще!"))
+
+print("COOL")
+    # day = int(input("Введите день рождения Пушкина: "))
+
+    # print(" В какой день родился А.С Пушкин?")f
+
+    # while day != 27:
+    # day = int(input("Попробуй еще!"))
+    # print('COOl')
+
+print(age)
+
